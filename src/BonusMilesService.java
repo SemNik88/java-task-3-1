2a887525f4bd5ccdf1ab1e6cf73a68a408d2ac6b
SemNik <semnikg@gmail.com>
2023-11-06 18:11:06 +0300

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,13 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+
+            int bonus;
+            if (cost >= 20) {
+                bonus = cost / 20;
+            } else {
+                bonus = 0;
+            }
+            return bonus;
+
+    }
+}
